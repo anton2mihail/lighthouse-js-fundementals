51ca93eb63184de2e76e95fc70d834335a5c0d14
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-10-02 17:47:19 -0400

    Adds conditionals exercises

---
+++ hundreds.js
@@ -0,0 +1,9 @@
+function howManyHundreds(num){
+  return Math.floor(num / 100);
+}
+
+
+console.log(howManyHundreds(1234));
+console.log(howManyHundreds(0));
+console.log(howManyHundreds(12));
+console.log(howManyHundreds(55));
