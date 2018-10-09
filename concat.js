7287baffe598de676dd65d60f66a84907aee8260
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-10-09 11:43:35 -0400

    Added Misc Exercises

---
+++ concat.js
@@ -0,0 +1,12 @@
+function concat(ar1, ar2){
+    var res = [];
+    res = ar1;
+    for(var i = 0; i < ar2.length ; i++){
+      res.push(ar2[i]);
+    }
+    return res;
+}
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
---
+++ merge.js
@@ -0,0 +1,8 @@
+function merge(ar1, ar2){
+  var res = ar1.concat(ar2);
+  res = res.sort();
+  return res;
+}
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
